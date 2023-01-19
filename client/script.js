0eae05075b1804d46780cdef71dc0e7014851ee7
aroana <erajeon@gmail.com>
2023-01-19 18:27:39 +0900

    update the servef url

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://codex-openai-iewo.onrender.com', {
+    const response = await fetch('https://codex-openai-lfdz.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
